ddc6d1852de2753459577d6a11ac8c434ec1cce7
Pratyusha Jayavarapu <Pjayavarapu3016@conestogac.on.ca>
2024-03-21 22:44:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Vehicle_rental__management_Assign3;
+namespace Vehicle_rental__management_Assign3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Civic", "Honda", 2018, 40.5m, 4, "Gasoline", "Automatic", true);
+         Truck truck = new Truck("F-150", "Ford", 2022, 60.0m, 1000, "Diesel", true);
+        Motorcycle motorcycle = new Motorcycle("Ninja 400", "Kawasaki", 2020, 20.0m, 399, "Gasoline", false);
+        RentalAgency agency = new RentalAgency(10);
+        agency.AddVehicle(car);
+        agency.AddVehicle(truck);
+        agency.AddVehicle(motorcycle);
+        agency.RentVehicle(car);
+        motorcycle.DisplayDetails();
+    }
+}
+    
---
+++ Rental-Agency.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_rental__management_Assign3
+{
+    public class RentalAgency
+    {
+        private Vehicle[] Fleet { get; set; }
+        public decimal TotalRevenue { get; private set; }
+
+        public RentalAgency(int fleetSize)
+        {
+            Fleet = new Vehicle[fleetSize];
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            for (int i = 0; i < Fleet.Length; i++)
+            {
+                if (Fleet[i] == null)
+                {
+                    Fleet[i] = vehicle;
+                    return;
+                }
+            }
+
+            Console.WriteLine("The fleet is full.");
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            for (int i = 0; i < Fleet.Length; i++)
+            {
+                if (Fleet[i] != null && Fleet[i].GetType() == vehicle.GetType())
+                {
+                    Fleet[i] = null;
+                    return;
+                }
+            }
+
+            Console.WriteLine("The vehicle is not in the fleet.");
+        }
+
+        public void RentVehicle(Vehicle vehicle)
+        {
+            if (Fleet.Any(v => v == vehicle))
+            {
+                Console.WriteLine("Vehicle rented.");
+                TotalRevenue += vehicle.RentalPrice;
+            }
+            else
+            {
+                Console.WriteLine("The vehicle is not in the fleet.");
+            }
+        }
+    }
+}
