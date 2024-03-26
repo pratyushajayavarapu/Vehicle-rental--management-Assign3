84e7e93dabef53f32571eb1f23cdd83f05741189
Pratyusha Jayavarapu <Pjayavarapu3016@conestogac.on.ca>
2024-03-25 20:39:17 -0400

    written  comments to few parts of the code and made some minute changes

--- Vehicle.cs
+++ Vehicle.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Vehicle class represents a generic vehicle with common properties.
+public class Vehicle
 namespace Vehicle_rental__management_Assign3
 {
+    // Constructor for creating a Vehicle object
     public class Vehicle
     {
         public string Model { get; set; }
         public string Manufacturer { get; set; }
         public int Year { get; set; }
         public decimal RentalPrice { get; set; }
-
+        //method to display details of the vehicle.
         public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
         {
             Model = model;
